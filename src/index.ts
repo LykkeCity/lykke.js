2615c5fe683da1ba315aa3ebfdff99e12b758dbc
GeorgePuisha <e.puysha@itransition.com>
2018-09-24 15:10:41 +0300

    Reorganize exports

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import {buildDijkstra} from './algorithm/dijkstra';
-import {track} from './analytics/track';
-import {formatNumber} from './intl/number';
-import * as safeMath from './safeMath/safeMath';
-
 import Amplitude from './analytics/Amplitude';
 import Analytics from './analytics/Analytics';
 import GoogleAnalytics from './analytics/GoogleAnalytics';
 import EventDetailsModel from './analytics/models/EventDetailsModel';
 import EventModel from './analytics/models/EventModel';
 import Segment from './analytics/Segment';
+import {track} from './analytics/track';
+import {formatNumber} from './intl/number';
+import * as safeMath from './safeMath/safeMath';
 
-export {buildDijkstra, formatNumber, safeMath, track};
 export {
+  buildDijkstra,
+  formatNumber,
+  safeMath,
+  track,
   Analytics,
   Amplitude,
   GoogleAnalytics,
